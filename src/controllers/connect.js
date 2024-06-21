6e73e5b6dc492a777ee3550a031c1396aa5e6af1
Krishna Teja Mekala <krishna18developer@gmail.com>
2024-06-21 20:53:14 +0530

    Reconfig

--- src/controllers/connect.js
+++
@@ -1,7 +0,0 @@
-module.exports.handler = async event => {
-    return {
-      statusCode: 200,
-      body: 'Connected.',
-    };
-  };
-  
--- src/controllers/default.js
+++
@@ -1,9 +0,0 @@
-module.exports.handler = async event => {
-    const message = JSON.parse(event.body);
-    // Handle incoming messages
-    return {
-      statusCode: 200,
-      body: 'Message received.',
-    };
-  };
-  
